e1e46d1c99c6c8328fd0f729bfa804364559c9e8
Mireno <mirenod@hotmail.com>
2020-02-10 11:51:33 +0000

    name

--- src/feature1/Feature1Component.js
+++ src/feature1/Feature1Component.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { getUsers, resetUsers } from "./actions";
 
-const Feature1Component = ({ getUsers, resetUsers, users }) => {
-  const handleOnClick = async () => {
-    getUsers();
-  };
+const Feature1Component = () => {
+  const users = useSelector(state => state.feature1.users);
+  const dispatch = useDispatch();
 
   return (
     <>
       <div>
         <h3>Feature 1:</h3>
-        <button onClick={handleOnClick}>Get Users API 1</button>
+        <button onClick={() => dispatch(getUsers())}>Get Users API 1</button>
         <button
           onClick={() => {
-            resetUsers();
+            dispatch(resetUsers());
           }}
         >
           Reset Users
@@ -28,18 +26,4 @@ const Feature1Component = ({ getUsers, resetUsers, users }) => {
   );
 };
 
-const mapStateToProps = state => ({
-  users: state.feature1.users
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      getUsers,
-      resetUsers
-    },
-    dispatch
-  );
-
-export default connect(mapStateToProps, mapDispatchToProps)(Feature1Component);
-// https://the-one-api.herokuapp.com/v1/book
+export default Feature1Component;
